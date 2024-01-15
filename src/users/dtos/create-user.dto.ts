9ff50391544244ae2d6cc32966c5b3a9a735bee9
Eliezer Nsengi <nsengitech@gmail.com>
2024-01-15 15:46:54 +0200

    add: class validator implementation

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(2)
+  password: string;
+}
