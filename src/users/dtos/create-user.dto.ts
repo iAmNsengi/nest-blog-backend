04a1cb9b45c2a40252a31d5557e0648524d3847a
Eliezer Nsengi <nsengitech@gmail.com>
2024-01-16 15:52:35 +0200

    add: class validator password match

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,18 +1,29 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  MinLength
+} from 'class-validator';
 
 export class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
+  @MinLength(3)
   firstName: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
+  @MinLength(3)
   lastName: string;
 
   @IsEmail()
   email: string;
 
   @IsString()
-  @Length(2)
+  @Length(8, 96)
+  @Matches(/^(?=.*[A-Zz-z])(?=.*\d)(?=.[@$!%#?&])[A-Za-z\d@$!%#?&]{8,}$/)
   password: string;
 }
