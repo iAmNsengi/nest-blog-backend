110e57d1c286d3a0c2c994a6bfdde6019eb58474
Eliezer Nsengi <nsengitech@gmail.com>
2024-01-20 15:39:07 +0200

    add: auto type conversion using Type from class-transformer

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe
+} from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
+import { GetUsersParamDTO } from './dtos/get-users-params.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,6 +16,12 @@ export class UsersController {
     return 'You are on get all users endpoint';
   }
 
+  @Get('/:id')
+  public getUserById(@Param() getUserParamDto: GetUsersParamDTO) {
+    console.log(getUserParamDto);
+
+    return 'You sent a get request to get user by Id';
+  }
   @Post()
   addUser(@Body() createUserDTO: CreateUserDTO) {
     return createUserDTO;
