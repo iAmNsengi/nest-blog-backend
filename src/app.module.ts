92fb35ab038d413492d0535d57f3ab143c942b78
Eliezer Nsengi <nsengitech@gmail.com>
2024-02-10 10:31:14 +0200

    add: posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService]
 })
