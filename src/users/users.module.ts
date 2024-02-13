19716e9ed09dcc62fa4db263115f7ac1d49cfa67
Eliezer Nsengi <nsengitech@gmail.com>
2024-02-13 10:37:43 +0200

    fix: circular dependency in auth module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,5 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './providers/users.services';
 
-@Module({ controllers: [UsersController], providers: [UsersService] })
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService]
+})
 export class UsersModule {}
