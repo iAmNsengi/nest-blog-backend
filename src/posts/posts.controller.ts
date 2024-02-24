e2ddf384777ce4331a4bdcf85a9b89ffddde41de
Eliezer Nsengi <nsengitech@gmail.com>
2024-02-24 15:31:03 +0200

    add: @ApiResponse and @ApiOperation descriptions

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDTO } from './dtos/create-post.dto';
 
 @ApiTags('Posts')
@@ -18,6 +18,10 @@ export class PostsController {
     return 'Get single post';
   }
 
+  @ApiOperation({ description: 'Create a new Blop post' })
+  @ApiResponse({ status: 201, description: 'Post created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 500, description: 'Internal server error' })
   @Post()
   public createPost(@Body() createPostDTO: CreatePostDTO) {
     return this.postService.createPost(createPostDTO);
