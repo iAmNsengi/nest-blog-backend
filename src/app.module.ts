e59ad720920f84d467c9c59445b42ad97260ebf0
Eliezer Nsengi <nsengitech@gmail.com>
2024-03-07 10:25:59 +0200

    add: user entity class

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     PostsModule,
     AuthModule,
     TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
       useFactory: () => ({
         type: 'postgres',
         entities: [],
