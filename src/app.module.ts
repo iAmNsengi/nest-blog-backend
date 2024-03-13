d0678b16fa4ad832d87a3e0a8c79723557e7c3bd
Eliezer Nsengi <nsengitech@gmail.com>
2024-03-13 08:43:27 +0200

    chore: create meta-options module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { TagsModule } from './tags/tags.module';
+import { MetaOptionsModule } from './meta-options/meta-options.module';
 
 @Module({
   imports: [
@@ -27,7 +28,8 @@ import { TagsModule } from './tags/tags.module';
         database: 'blog-database'
       })
     }),
-    TagsModule
+    TagsModule,
+    MetaOptionsModule
   ],
   controllers: [AppController],
   providers: [AppService]
