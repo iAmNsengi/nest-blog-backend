da272c85b3906b375c4958fd396c436d6db1d411
Eliezer Nsengi <nsengitech@gmail.com>
2024-05-07 10:23:16 +0200

    add: inject metaoption repository and create new metaoption

--- src/meta-options/providers/meta-options.service.ts
+++ src/meta-options/providers/meta-options.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostMetaOptionsDTO } from '../dtos/create-post-meta-options.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOption } from '../meta-option.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MetaOptionsService {}
+export class MetaOptionsService {
+  constructor(
+    /**
+     * Injecting meta options repository
+     */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>
+  ) {}
+
+  public async create(createPostMetaOptionsDTO: CreatePostMetaOptionsDTO) {
+    const metaOption = this.metaOptionsRepository.create(
+      createPostMetaOptionsDTO
+    );
+    return await this.metaOptionsRepository.save(metaOption);
+  }
+}
