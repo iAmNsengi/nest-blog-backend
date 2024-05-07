aff00c831e952a74a623a813eff7cc2b549de132
Eliezer Nsengi <nsengitech@gmail.com>
2024-05-07 10:29:58 +0200

    add: get all and post meta option

--- src/meta-options/meta-options.controller.ts
+++ src/meta-options/meta-options.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiProperty, ApiResponse } from '@nestjs/swagger';
 
 @Controller('meta-options')
-export class MetaOptionsController {}
+export class MetaOptionsController {
+  @Get()
+  @ApiOperation({ summary: 'Get all meta options' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved meta options'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'A bad request was detected'
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An internal server error occured'
+  })
+  public getAll() {
+    return 'This is the get all endpoint';
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new meta option' })
+  public createMetaOption() {
+    return 'Create a new meta option';
+  }
+}
