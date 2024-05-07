8ec2b335d6ee5fac4cd9426b23f91614ceecc2b8
Eliezer Nsengi <nsengitech@gmail.com>
2024-05-07 10:35:39 +0200

    add: create new metaoption

--- src/meta-options/dtos/create-post-meta-options.dto.ts
+++ src/meta-options/dtos/create-post-meta-options.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsJSON, IsNotEmpty } from 'class-validator';
 
 export class CreatePostMetaOptionsDTO {
+  @ApiProperty()
   @IsNotEmpty()
   @IsJSON()
   metaValue: string;
--- src/meta-options/meta-options.controller.ts
+++ src/meta-options/meta-options.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiOperation, ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { CreatePostMetaOptionsDTO } from './dtos/create-post-meta-options.dto';
+import { MetaOptionsService } from './providers/meta-options.service';
 
 @Controller('meta-options')
 export class MetaOptionsController {
+  constructor(
+    /** Inject metaOption service*/
+    public readonly metaOptionService: MetaOptionsService
+  ) {}
+
   @Get()
   @ApiOperation({ summary: 'Get all meta options' })
   @ApiResponse({
@@ -23,7 +30,21 @@ export class MetaOptionsController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new meta option' })
-  public createMetaOption() {
-    return 'Create a new meta option';
+  @ApiResponse({
+    status: 201,
+    description: 'New Meta Option created successfully!'
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request'
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An internal server error was detected!'
+  })
+  public createMetaOption(
+    @Body() createMetaOptionDTO: CreatePostMetaOptionsDTO
+  ) {
+    return this.metaOptionService.create(createMetaOptionDTO);
   }
 }
