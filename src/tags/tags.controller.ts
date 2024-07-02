1d1625b9c028f472095e7de4939ee853cddccfe3
Eliezer Nsengi <nsengitech@gmail.com>
2024-07-02 18:12:14 +0200

    ft: create new tag && get all tags

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTagDTO } from './dtos/create-tag.dto';
+import { TagsService } from './tags.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagService: TagsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get all tags' })
+  public getAllTags() {
+    return this.tagService.getAllTags();
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new tag' })
+  public createTag(@Body() createTagDTO: CreateTagDTO) {
+    return this.tagService.createTag(createTagDTO);
+  }
+}
