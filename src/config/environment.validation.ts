0920e633cbac19e0928a4d56314cbdfb2c095d2a
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-09 17:42:31 +0200

    ft: Joi object set up

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -1,5 +1,12 @@
 import * as Joi from 'joi';
 
 export default Joi.object({
-  NODE_ENV: Joi.string().valid('development', 'production')
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'staging')
+    .default('development'),
+  DATABASE_PORT: Joi.number().port().default(5432),
+  DATABASE_NAME: Joi.string().required(),
+  DATABASE_HOST: Joi.string().default('localhost'),
+  DATABASE_USER: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required()
 });
