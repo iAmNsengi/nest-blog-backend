2fd9efa3fdc5e5c162bf6d061f72c65dc78aa78d
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-09 15:00:41 +0200

    ft: setting up profile environment

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,17 @@ import { UsersService } from './providers/users.services';
 import { AuthModule } from 'src/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { ConfigModule } from '@nestjs/config';
+import profileConfig from './config/profileConfig';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
-  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])]
+  imports: [
+    forwardRef(() => AuthModule),
+    TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(profileConfig)
+  ]
 })
 export class UsersModule {}
