ce0b3bb37328f423196701ef5ac57cd48c0e00a1
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-11 12:33:18 +0200

    add: create many users provider

---
+++ src/users/providers/users-create-many.provider.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from '../dtos/create-user.dto';
+import { User } from '../user.entity';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class UsersCreateManyProvider {
+  constructor(private readonly dataSource: DataSource) {}
+
+  public async createManyUsers(createUsersDTO: CreateUserDTO[]) {
+    let newUsers: User[] = [];
+    // create a query runner instance
+    const queryRunner = this.dataSource.createQueryRunner();
+    // connect query runner to our data source
+    await queryRunner.connect();
+    // start transaction
+    await queryRunner.startTransaction();
+
+    try {
+      for (let user of createUsersDTO) {
+        let newUser = queryRunner.manager.create(User, user);
+        let result = await queryRunner.manager.save(newUser);
+        newUsers.push(result);
+        // if successful commit the transaction
+        await queryRunner.commitTransaction();
+      }
+    } catch (error) {
+      // if transaction unsuccessful rollback
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // release back the connection
+      await queryRunner.release();
+    }
+  }
+}
--- src/users/providers/users.services.ts
+++ src/users/providers/users.services.ts
@@ -78,29 +78,5 @@ export class UsersService {
     return newUser;
   }
 
-  public async createManyUsers(createUsersDTO: CreateUserDTO[]) {
-    let newUsers: User[] = [];
-    // create a query runner instance
-    const queryRunner = this.dataSource.createQueryRunner();
-    // connect query runner to our data source
-    await queryRunner.connect();
-    // start transaction
-    await queryRunner.startTransaction();
-
-    try {
-      for (let user of createUsersDTO) {
-        let newUser = queryRunner.manager.create(User, user);
-        let result = await queryRunner.manager.save(newUser);
-        newUsers.push(result);
-        // if successful commit the transaction
-        await queryRunner.commitTransaction();
-      }
-    } catch (error) {
-      // if transaction unsuccessful rollback
-      await queryRunner.rollbackTransaction();
-    } finally {
-      // release back the connection
-      await queryRunner.release();
-    }
-  }
+  
 }
