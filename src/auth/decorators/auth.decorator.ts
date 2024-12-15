e08e5dfe04e0c17dd21c754044c154319898be4a
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-15 20:59:18 +0200

    add: auth type decorator

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,3 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
+import { AUTH_TYPE_KEY } from '../constants/auth.constants';
+import { AuthType } from '../enums/auth-type.enum';
 
-export const Auth = (...args: string[]) => SetMetadata('auth', args);
+// auth is a decorator which takes in only two types of arguments by now
+export const Auth = (...authTypes: AuthType[]) =>
+  SetMetadata(AUTH_TYPE_KEY, authTypes);
