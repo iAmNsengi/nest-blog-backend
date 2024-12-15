f7b88dd7167c199eb0d6025a3b97e7cfdd2632ee
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-15 16:48:17 +0200

    feat: protecting the users module globally with a guard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -11,6 +11,8 @@ import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email.p
 import profileConfig from './config/profileConfig';
 import jwtConfig from 'src/auth/config/jwt.config';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from 'src/auth/guards/access-token/access-token.guard';
 
 @Module({
   controllers: [UsersController],
@@ -18,7 +20,11 @@ import { JwtModule } from '@nestjs/jwt';
     UsersService,
     UsersCreateManyProvider,
     CreateUserProvider,
-    FindOneUserByEmailProvider
+    FindOneUserByEmailProvider,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard
+    }
   ],
   exports: [UsersService],
   imports: [
