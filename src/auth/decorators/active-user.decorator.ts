b026249fef0ade3f95715696fda8122c544c7def
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-16 09:29:41 +0200

    create: active user interface

--- src/auth/decorators/active-user.decorator.ts
+++ src/auth/decorators/active-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
+export const ActiveUser = createParamDecorator(
+  (field: , ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
 
---
+++ src/auth/interfaces/active-user-interface.ts
@@ -0,0 +1,5 @@
+export interface ActiveUser {
+  // sub is id user in the database
+  sub: number;
+  email: string;
+}
