9d4482652844b9ef59417ea830dc3fb742ddd7fa
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-16 19:07:18 +0200

    dependency injection refresh tokens

--- src/auth/providers/refresh-tokens.provider.ts
+++ src/auth/providers/refresh-tokens.provider.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { RefreshTokenDTO } from '../dtos/refresh-token.dto';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
-export class RefreshTokensProvider {}
+export class RefreshTokensProvider {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>
+  ) {}
+  public async refreshTokens(refreshTokenDTO: RefreshTokenDTO) {
+    // verify the refresh token using jwt service
+  }
+}
