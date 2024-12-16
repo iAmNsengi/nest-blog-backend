135e3d32a5ceb42591bd68b304fc8c1c72a443c6
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-16 18:02:34 +0200

    add: dependency injection

--- src/auth/providers/generate-tokens.provider.ts
+++ src/auth/providers/generate-tokens.provider.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class GenerateTokensProvider {}
+export class GenerateTokensProvider {
+  constructor(
+    /** injecting the jwt configs */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    /** injecting the jwt service */
+    private readonly jwtService: JwtService
+  ) {}
+}
