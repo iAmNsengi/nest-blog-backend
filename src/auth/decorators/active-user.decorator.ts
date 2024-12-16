e432ce65555e2c55c7ed37454d669779d0151a2e
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-16 09:32:57 +0200

    add: active user decorator wrap up

--- src/auth/decorators/active-user.decorator.ts
+++ src/auth/decorators/active-user.decorator.ts
@@ -1,10 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { ActiveUserInterface } from '../interfaces/active-user-interface';
+import { REQUEST_USER_KEY } from '../constants/auth.constants';
 
 export const ActiveUser = createParamDecorator(
-  (field: , ctx: ExecutionContext) => {
+  (field: keyof ActiveUserInterface | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const user = request.user;
+    const user: ActiveUserInterface = request[REQUEST_USER_KEY];
 
-    return data ? user?.[data] : user;
+    return field ? user?.[field] : user;
   }
 );
--- src/auth/interfaces/active-user-interface.ts
+++ src/auth/interfaces/active-user-interface.ts
@@ -1,4 +1,4 @@
-export interface ActiveUser {
+export interface ActiveUserInterface {
   // sub is id user in the database
   sub: number;
   email: string;
