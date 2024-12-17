5e958e68298c242321ccdd94c3ed86ad957aa73e
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-17 15:08:02 +0200

    add: mail configs

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,8 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('appConfig', () => ({
   environment: process.env.NODE_ENV ?? 'production',
-  apiVersion: process.env.API_VERSION
+  apiVersion: process.env.API_VERSION,
+  mailHost: process.env.MAIL_HOST,
+  mailSMTPUsername: process.env.MAIL_SMTP_USERNAME,
+  mailSMTPPassword: process.env.MAIL_SMTP_PASSWORD
 }));
