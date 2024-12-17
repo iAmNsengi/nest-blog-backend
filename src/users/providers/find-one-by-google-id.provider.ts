42b29a3ea77d2ae88007fe95ddf3196227107ded
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-17 08:49:04 +0200

    add: findByGoogleId method && DI

--- src/users/providers/find-one-by-google-id.provider.ts
+++ src/users/providers/find-one-by-google-id.provider.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user.entity';
+import { Repository } from 'typeorm';
+import requestTimeoutError from 'src/errors/RequestTimeout';
 
 @Injectable()
-export class FindOneByGoogleIdProvider {}
+export class FindOneByGoogleIdProvider {
+  constructor(
+    /**inject  user repository*/
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>
+  ) {}
+
+  public async findUserByGoogleId(googleId: string) {
+    try {
+      return await this.usersRepository.findOneBy({ googleId });
+    } catch (error) {
+      requestTimeoutError();
+    }
+  }
+}
