083c7373c2a819cedb4ef02175ee8fce876399e1
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-17 08:32:05 +0200

    add: implement onModuleInit

--- src/auth/social/providers/google-authentication/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication/google-authentication.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  private oauthClient: OAuth2Client;
+
+  constructor(
+    /** Inject jwtConfiguration */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>
+    ) { }
+    
+    onModuleInit() {
+        const clientId = this.jwtConfiguration.googleAuthId'
+        const clientSecret = this.jwtConfiguration.googleAuthSecret
+        this.oauthClient = new OAuth2Client(clientId,clientSecret)
+    }
+}
