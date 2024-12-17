678634b76abb6031ef0d05f6c71651c2b09073fe
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-17 15:44:16 +0200

    ft: config the mail service provider

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,4 +1,25 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { name } from 'ejs';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(
+    /** inject the mailservice */
+    private mailerService: MailerService
+  ) {}
+  public async sendUserWelcome(user: User) {
+    await this.mailerService.sendMail({
+      to: user.email,
+      from: `Onboarding Team <support@zerblog.com>`,
+      subject: `Welcome to ZerBlog`,
+      template: './welcome',
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: `http://localhost:3000`
+      }
+    });
+  }
+}
