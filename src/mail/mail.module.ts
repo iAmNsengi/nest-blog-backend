d638f5b81c341441cbca58c1d872163e16d08ffe
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-17 15:29:31 +0200

    add: mailer configuration

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MailService } from './providers/mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { strict } from 'assert';
 
+@Global()
 @Module({
-  providers: [MailService]
+  providers: [MailService],
+  exports: [MailService],
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('appConfig.mailHost'),
+          secure: false,
+          port: 2525,
+          auth: {
+            user: config.get('appConfig.mailSMTPUser'),
+            password: config.get('appConfig.mailSMTPPassword')
+          }
+        },
+        default: {
+          from: `ZerBlog <no-reply@zerblog.com>`
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new EjsAdapter(),
+          options: { strict: false }
+        }
+      })
+    })
+  ]
 })
 export class MailModule {}
