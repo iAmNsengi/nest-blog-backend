2de0bc599ec91b25e6e12d3aec0a04454cd5a2d6
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-17 10:54:03 +0200

    ft: exception handling create google user

--- src/auth/social/providers/google-authentication/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication/google-authentication.service.ts
@@ -1,4 +1,10 @@
-import { forwardRef, Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  OnModuleInit,
+  UnauthorizedException
+} from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { OAuth2Client } from 'google-auth-library';
 import jwtConfig from 'src/auth/config/jwt.config';
@@ -27,33 +33,38 @@ export class GoogleAuthenticationService implements OnModuleInit {
     this.oauthClient = new OAuth2Client(clientId, clientSecret);
   }
   public async authenticate(googleTokenDTO: GoogleTokenDTO) {
-    //verify the google token sent by user
-    const loginTicket = await this.oauthClient.verifyIdToken({
-      idToken: googleTokenDTO.token
-    });
+    try {
+      //verify the google token sent by user
+      const loginTicket = await this.oauthClient.verifyIdToken({
+        idToken: googleTokenDTO.token
+      });
 
-    // extract the payload from Google JWT
-    const {
-      email,
-      sub: googleId,
-      family_name: lastName,
-      given_name: firstName
-    } = loginTicket.getPayload();
-    // Find the user in the databse using the googleId
-    const user = await this.usersService.findOneByUserGoogleId(googleId);
-    if (user) {
-      return await this.generateTokensProvider.generateTokens(user);
+      // extract the payload from Google JWT
+      const {
+        email,
+        sub: googleId,
+        family_name: lastName,
+        given_name: firstName
+      } = loginTicket.getPayload();
+      // Find the user in the databse using the googleId
+      const user = await this.usersService.findOneByUserGoogleId(googleId);
+      if (user) {
+        return await this.generateTokensProvider.generateTokens(user);
+      }
+
+      // If not create a new user and then generate tokens
+      const newUser = await this.usersService.createUser({
+        firstName,
+        lastName,
+        email,
+        password: '',
+        googleId
+      });
+
+      if (newUser)
+        return await this.generateTokensProvider.generateTokens(newUser);
+    } catch (error) {
+      throw new UnauthorizedException();
     }
-
-    // If not create a new user and then generate tokens
-    const newUser = await this.usersService.createUser({
-      firstName,
-      lastName,
-      email,
-      password: '',
-      googleId
-    });
-
-    // Throw unauthorized exception
   }
 }
