abf97d81b83f99fce04bf4442d3d32e3bb5221b7
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-18 08:38:51 +0200

    ft: use transactions and datasource to create a new user and send confirmation email

--- src/users/providers/create-user.provider.ts
+++ src/users/providers/create-user.provider.ts
@@ -2,18 +2,20 @@ import {
   BadRequestException,
   forwardRef,
   Inject,
-  Injectable
+  Injectable,
+  InternalServerErrorException
 } from '@nestjs/common';
 import { CreateUserDTO } from '../dtos/create-user.dto';
 import { CreateManyUsersDTO } from '../dtos/create-many-user.dto';
 import requestTimeoutError from 'src/errors/RequestTimeout';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from '../user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersCreateManyProvider } from './users-create-many.provider';
 import { HashingProvider } from 'src/auth/providers/hashing.provider';
 import { create } from 'domain';
 import { MailService } from 'src/mail/providers/mail.service';
+import { log } from 'console';
 
 @Injectable()
 export class CreateUserProvider {
@@ -27,37 +29,61 @@ export class CreateUserProvider {
     @Inject(forwardRef(() => HashingProvider))
     private readonly hashingProvider: HashingProvider,
     /** injecting mailService */
-    private readonly mailService: MailService
+    private readonly mailService: MailService,
+    /** injecting the datasource */
+    private readonly dataSource: DataSource
   ) {}
 
   public async createUser(createUserDTO: CreateUserDTO) {
+    const queryRunner = this.dataSource.createQueryRunner();
     let userExists = undefined;
     try {
-      userExists = await this.usersRepository.findOne({
+      // connecting to the queryRunner
+      await queryRunner.connect();
+
+      // start the transaction
+      await queryRunner.startTransaction();
+
+      //check whether the user already exists
+      userExists = await queryRunner.manager.findOne(User, {
         where: {
           email: createUserDTO.email
         }
       });
-    } catch (error) {
-      requestTimeoutError();
-    }
-    if (userExists)
-      throw new BadRequestException('User with email already exists');
 
-    let newUser = this.usersRepository.create({
-      ...createUserDTO,
-      password: await this.hashingProvider.hashPassword(createUserDTO.password)
-    });
-    try {
-      await this.usersRepository.save(newUser);
-    } catch (error) {
-      requestTimeoutError();
-    }
-    try {
+      if (userExists)
+        throw new BadRequestException('User with email already exists');
+
+      // use query manager to create a new user
+      let newUser = queryRunner.manager.create(User, {
+        ...createUserDTO,
+        password: await this.hashingProvider.hashPassword(
+          createUserDTO.password
+        )
+      });
+
+      // save the created user
+      await queryRunner.manager.save(newUser);
+
+      // send a confirmation email
       await this.mailService.sendUserWelcome(newUser);
+
+      // committing the transaction
+      await queryRunner.commitTransaction();
+
+      // return the created user
+      return newUser;
     } catch (error) {
-      requestTimeoutError();
+      // on any error rollback the transaction
+      await queryRunner.rollbackTransaction();
+
+      // log the error
+      console.log(error);
+
+      // throw an error
+      throw new InternalServerErrorException(
+        "Couldn't create the user at the moment, Try again later."
+      );
     }
-    return newUser;
   }
 }
