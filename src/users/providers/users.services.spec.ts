032973da44dfa1bb074d381aeaf6d06ea5e1d579
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-18 11:46:43 +0200

    ft: add create user unit test

--- src/users/providers/users.services.spec.ts
+++ src/users/providers/users.services.spec.ts
@@ -7,10 +7,25 @@ import { UsersCreateManyProvider } from './users-create-many.provider';
 import { DataSource } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../user.entity';
+import { CreateUserDTO } from '../dtos/create-user.dto';
+import { create } from 'domain';
+import exp from 'constants';
 
 describe('UsersService', () => {
   let service: UsersService;
   beforeEach(async () => {
+    // Just testing if the createUserProvider has a method to create user and if it fires it on request
+    const mockCreateUserProvider: Partial<CreateUserProvider> = {
+      createUser: (createUserDTO: CreateUserDTO) =>
+        Promise.resolve({
+          id: 12,
+          firstName: createUserDTO.firstName,
+          lastName: createUserDTO.lastName,
+          email: createUserDTO.email,
+          password: createUserDTO.password
+        })
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
@@ -39,6 +54,22 @@ describe('UsersService', () => {
   describe('root', () => {
     it('Service should be defined', () => {
       expect(service).toBeDefined();
+    });
+  });
+
+  describe('CreateUser', () => {
+    it('Should be defined', () => {
+      expect(service.createUser).toBeDefined();
+    });
+
+    it('Should call create user on CreateUserProvider', async () => {
+      let user = await service.createUser({
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@doe.com',
+        password: 'password'
+      });
+      expect(user.firstName).toEqual('John');
     });
   });
 });
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -47,5 +47,5 @@ export class User {
   googleId?: string;
 
   @OneToMany(() => Post, (post) => post.author)
-  posts: Post[];
+  posts?: Post[];
 }
